d5db16c8cf111575785c16459f283ed1e766d68a
rajmanna <dev.rajm8923@gmail.com>
2025-09-23 10:11:08 +0530

    Improved requests count visibility

--- Client/src/components/NotificationIndicator.jsx
+++ Client/src/components/NotificationIndicator.jsx
@@ -2,13 +2,19 @@ import { useFriendRequests } from "@/queries/friendQueries";
 
 function NotificationIndicator({ size = 5, visibility = true }) {
   const { data: requests = [] } = useFriendRequests();
-  const isPendingRequests = requests.length !== 0;
+  const count = requests.length;
 
-  if (!isPendingRequests) return null;
+  if (!count) return null;
 
   return (
-    <div className={`h-${size} w-${size} text-sm rounded-full bg-green-500`}>
-      {visibility && requests.length}
+    <div
+      className={`h-${size} w-${size} flex justify-center items-center p-2 text-xs rounded-full bg-green-500`}
+    >
+      {visibility && (
+        <span className="flex items-center">
+          {count > 9 ? `${count}+` : count}
+        </span>
+      )}
     </div>
   );
 }
