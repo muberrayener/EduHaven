460d13ac0fab068637183706a8b766df534d154f
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-09-30 19:36:34 +0530

    Update winstonLogger.js

--- Server/logger/winstonLogger.js
+++ Server/logger/winstonLogger.js
@@ -4,35 +4,35 @@ import path from "path";
 const logDir = "logs";
 
 const logger = winston.createLogger({
-    level: "info",
-    format: winston.format.combine(
-        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-        winston.format.errors({ stack: true }),
-        winston.format.json()
-    ),
-    transports: [
-        new winston.transports.File({
-            filename: path.join(logDir, "error.log"),
-            level: "error",
-        }),
-        new winston.transports.File({
-            filename: path.join(logDir, "combined.log"),
-        }),
-    ],
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: path.join(logDir, "error.log"),
+      level: "error",
+    }),
+    new winston.transports.File({
+      filename: path.join(logDir, "combined.log"),
+    }),
+  ],
 });
 
 if (process.env.NODE_ENV !== "production") {
-    logger.add(
-        new winston.transports.Console({
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.printf(
-                    ({ level, message, timestamp, stack }) =>
-                    `${timestamp} ${level}: ${stack || message}`
-                )
-            ),
-        })
-    );
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf(
+          ({ level, message, timestamp, stack }) =>
+            `${timestamp} ${level}: ${stack || message}`
+        )
+      ),
+    })
+  );
 }
 
 export default logger;
