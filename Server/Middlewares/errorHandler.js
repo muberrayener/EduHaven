0604190d71c775189b3a3ba5bcbeaa4a88d2b344
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-09-30 19:37:28 +0530

    Update errorHandler.js

--- Server/Middlewares/errorHandler.js
+++ Server/Middlewares/errorHandler.js
@@ -1,17 +1,17 @@
 import logger from "../logger/winstonLogger.js";
 
 const errorHandler = (err, req, res, next) => {
-    logger.error({
-        message: err.message || "Internal Server Error",
-        stack: err.stack,
-        route: req.originalUrl,
-        method: req.method,
-    });
+  logger.error({
+    message: err.message || "Internal Server Error",
+    stack: err.stack,
+    route: req.originalUrl,
+    method: req.method,
+  });
 
-    res.status(err.status || 500).json({
-        success: false,
-        message: err.message || "Internal Server Error",
-    });
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
 };
 
 export default errorHandler;
