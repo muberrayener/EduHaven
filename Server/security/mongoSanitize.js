77cb556792ea82ec1963f9caab8f7efcad62ab00
Nikhil Gupta <100pranjalgupta@gmail.com>
2025-10-07 16:42:14 +0530

    Update mongoSanitize.js

--- Server/security/mongoSanitize.js
+++ Server/security/mongoSanitize.js
@@ -1,10 +1,14 @@
-import mongoSanitize from 'express-mongo-sanitize';
+import mongoSanitize from "express-mongo-sanitize";
 
 export const mongoSecurity = (app) => {
-    app.use(mongoSanitize({
-        replaceWith: '_',
-        onSanitize: (req, res, next) => {
-            console.warn(`[MongoSanitize] Key sanitized: ${key} | Value: ${req.body[key]}`)
-        }
-    }))
-}
+  app.use(
+    mongoSanitize({
+      replaceWith: "_",
+      onSanitize: ({ req, key }) => {
+        console.warn(
+          `[MongoSanitize] Key sanitized: ${key} | Value: ${req.body[key]}`
+        );
+      },
+    })
+  );
+};
