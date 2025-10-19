9addaf3cefe7f620a73417192dbb6279e3dedee6
dattu145 <sridharreddy145233@gmail.com>
2025-10-19 23:31:48 +0530

    Changed the Model version to Gemini-2.0-Flash

--- Client/src/components/home/navBar/AiChatbot.jsx
+++ Client/src/components/home/navBar/AiChatbot.jsx
@@ -87,7 +87,7 @@ const Ai = () => {
 
     try {
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apikey}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apikey}`,
         {
           method: "POST",
           headers: {
